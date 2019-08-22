0ce8e1b9f1bb1117acbc83e650c71b9e3789a5fa
Pavel Shakhov <ab.pongo@gmail.com>
2019-08-22 20:56:32 +0300

    Перевел на assert. После ava и jest expect'ы chai'я выглядят хаосом

--- test/WeightUseCase.test.ts
+++ test/WeightUseCase.test.ts
@@ -1,12 +1,8 @@
-import chai, { expect } from 'chai';
+import { assert } from 'chai';
 import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
 import { validateWeight, WeightUseCase } from 'src/app/bot/WeightCommand/WeightUseCase';
 import { InvalidFormatError } from 'src/app/shared/errors';
 import { Kg, TelegramUserId } from 'src/app/shared/modelTypes';
-import { Result } from 'src/shared/utils/result';
-
-chai.use(sinonChai);
 
 const u = (id: number) => id as TelegramUserId;
 const kg = (value: number) => value as Kg;
@@ -19,33 +15,38 @@ describe('WeightUseCase', () => {
 
       const actual = await usecase.add(u(1), '10');
 
-      expect(actual.isOk).to.true;
-      expect(repository.add).calledOnceWith(u(1), kg(10));
+      assert.isTrue(actual.isOk);
+      sinon.assert.calledOnce(repository.add);
+      sinon.assert.calledWith(repository.add, u(1), kg(10));
     });
 
     it('should return error on invalid weight', async () => {
+      let plan = 0;
       const repository = { add: sinon.fake() };
       const usecase = new WeightUseCase(repository);
 
       const actual = await usecase.add(u(1), '');
 
-      expect(actual).deep.equal(Result.err(new InvalidFormatError()));
-      expect(repository.add, 'repository').not.called;
+      if (actual.isErr) {
+        assert.instanceOf(actual.error, InvalidFormatError);
+        plan++;
+      }
       sinon.assert.notCalled(repository.add);
+      assert.equal(plan, 1, 'plan');
     });
   });
 });
 
 describe('isValidWeight()', () => {
   it('value should be not null', () => {
-    expect(validateWeight(null)).to.null;
+    assert.isNull(validateWeight(null));
   });
 
   it('value should be >= 1 and <= 500', () => {
-    expect(validateWeight(0)).to.null;
-    expect(validateWeight(-100)).to.null;
-    expect(validateWeight(501)).to.null;
+    assert.isNull(validateWeight(0));
+    assert.isNull(validateWeight(-100));
+    assert.isNull(validateWeight(501));
 
-    expect(validateWeight(50.5)).to.equal(50.5);
+    assert.equal(validateWeight(50.5), kg(50.5));
   });
 });
