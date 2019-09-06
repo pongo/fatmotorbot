faa38ee864216e34bd82e3ee3b01504e4219af36
Pavel Shakhov <ab.pongo@gmail.com>
2019-09-06 03:20:32 +0300

    calcIdealWeight

---
+++ dist/src/app/bot/BMI/IdealWeight.js
@@ -0,0 +1,105 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const BMI_1 = require("src/app/bot/BMI/BMI");
+const parseNumber_1 = require("src/shared/utils/parseNumber");
+const stats_lite_1 = require("stats-lite");
+function broca(height, gender) {
+    const coeff = gender === 'male' ? 0.9 : 0.85;
+    const weight = (height - 100) * coeff;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.broca = broca;
+function brocaBrugsh(height) {
+    const coeff = height < 165 ? 100 : height > 175 ? 110 : 105;
+    const weight = height - coeff;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.brocaBrugsh = brocaBrugsh;
+function devine(height, gender) {
+    const coeff = gender === 'male' ? 50 : 45.5;
+    const weight = coeff + 2.3 * (0.393701 * height - 60);
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.devine = devine;
+function robinson(height, gender) {
+    const d = 0.393701 * height - 60;
+    const weight = gender === 'male' ? 52 + 1.9 * d : 49 + 1.7 * d;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.robinson = robinson;
+function miller(height, gender) {
+    const d = 0.393701 * height - 60;
+    const weight = gender === 'male' ? 56.2 + 1.41 * d : 53.1 + 1.36 * d;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.miller = miller;
+function hamwi(height, gender) {
+    const d = 0.393701 * height - 60;
+    const weight = gender === 'male' ? 48 + 2.7 * d : 45.5 + 2.2 * d;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.hamwi = hamwi;
+function lemmens(height) {
+    const weight = 22 * (height / 100) ** 2;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.lemmens = lemmens;
+function averageHealthyBMI(height, gender) {
+    const range = BMI_1.getHealthyRange(gender, height);
+    return parseNumber_1.roundToTwo(stats_lite_1.median(range));
+}
+exports.averageHealthyBMI = averageHealthyBMI;
+function lorenz(height, gender) {
+    const coeff = gender === 'male' ? 4 : 2;
+    const weight = height - 100 - (height - 150) / coeff;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.lorenz = lorenz;
+function mohammed(height) {
+    const weight = height * height * 0.00225;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.mohammed = mohammed;
+function insurance(height) {
+    const AGE = 25;
+    const weight = 50 + 0.75 * (height - 150) + (AGE - 20) / 4;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.insurance = insurance;
+function kuper(height, gender) {
+    const weight = gender === 'male' ? 0.713 * height - 58 : 0.624 * height - 48.9;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.kuper = kuper;
+function breitman(height) {
+    const weight = height * 0.7 - 50;
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.breitman = breitman;
+function tetona(height) {
+    const weight = height - (100 + (height - 100) / 20);
+    return parseNumber_1.roundToTwo(weight);
+}
+exports.tetona = tetona;
+const formulas = [
+    averageHealthyBMI,
+    breitman,
+    broca,
+    brocaBrugsh,
+    devine,
+    hamwi,
+    insurance,
+    kuper,
+    lemmens,
+    lorenz,
+    miller,
+    mohammed,
+    robinson,
+    tetona,
+];
+function calcIdealWeight(height, gender) {
+    const values = formulas.map(f => f(height, gender));
+    return parseNumber_1.roundToTwo(stats_lite_1.median(values));
+}
+exports.calcIdealWeight = calcIdealWeight;
+//# sourceMappingURL=IdealWeight.js.map
---
+++ test/nodb/BMI/IdealWeight.test.ts
@@ -0,0 +1,89 @@
+import { assert } from 'chai';
+import {
+  averageHealthyBMI,
+  breitman,
+  broca,
+  brocaBrugsh,
+  calcIdealWeight,
+  devine,
+  hamwi,
+  insurance,
+  kuper,
+  lemmens,
+  lorenz,
+  miller,
+  mohammed,
+  robinson,
+  tetona,
+} from 'src/app/bot/BMI/IdealWeight';
+import { cm, kg } from 'src/app/shared/types';
+
+describe('Ideal Weight calculation', () => {
+  it('broca', () => {
+    assert.equal(broca(cm(171), 'male'), kg(63.9));
+    assert.equal(broca(cm(171), 'female'), kg(60.35));
+  });
+
+  it('brocaBrugsh', () => {
+    assert.equal(brocaBrugsh(cm(171)), kg(66));
+  });
+
+  it('devine', () => {
+    assert.equal(devine(cm(171), 'male'), kg(66.84));
+    assert.equal(devine(cm(171), 'female'), kg(62.34));
+  });
+
+  it('robinson', () => {
+    assert.equal(robinson(cm(171), 'male'), kg(65.91));
+    assert.equal(robinson(cm(171), 'female'), kg(61.45));
+  });
+
+  it('miller', () => {
+    assert.equal(miller(cm(171), 'male'), kg(66.53));
+    assert.equal(miller(cm(171), 'female'), kg(63.06));
+  });
+
+  it('hamwi', () => {
+    assert.equal(hamwi(cm(171), 'male'), kg(67.77));
+    assert.equal(hamwi(cm(171), 'female'), kg(61.61));
+  });
+
+  it('lemmens', () => {
+    assert.equal(lemmens(cm(171)), kg(64.33));
+  });
+
+  it('averageHealthyBMI', () => {
+    assert.equal(averageHealthyBMI(cm(171), 'male'), kg(66.5));
+    assert.equal(averageHealthyBMI(cm(171), 'female'), kg(63.5));
+  });
+
+  it('lorenz', () => {
+    assert.equal(lorenz(cm(171), 'male'), kg(65.75));
+    assert.equal(lorenz(cm(171), 'female'), kg(60.5));
+  });
+
+  it('mohammed', () => {
+    assert.equal(mohammed(cm(171)), kg(65.79));
+  });
+
+  it('insurance', () => {
+    assert.equal(insurance(cm(171)), kg(67));
+  });
+
+  it('kuper', () => {
+    assert.equal(kuper(cm(171), 'male'), kg(63.92));
+    assert.equal(kuper(cm(171), 'female'), kg(57.8));
+  });
+
+  it('breitman', () => {
+    assert.equal(breitman(cm(171)), kg(69.7));
+  });
+
+  it('tetona', () => {
+    assert.equal(tetona(cm(171)), kg(67.45));
+  });
+
+  it('calcIdealWeight', () => {
+    assert.equal(calcIdealWeight(cm(171), 'male'), 66.25);
+  });
+});
