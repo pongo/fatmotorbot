5e466cda8f45a8ed306f9b87fc5130574efd9b56
Pavel Shakhov <ab.pongo@gmail.com>
2020-05-01 12:26:44 +0300

    Для ИДЕ сделал быстрый конфиг eslint

---
+++ .eslintrc.ide.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+module.exports = {
+  extends: '.eslintrc.js',
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/namespace': 0,
+    'import/no-cycle': 0,
+    'import/no-duplicates': 0,
+    'import/order': 0,
+    'import/named': 0,
+    'import/no-self-import': 0,
+    'import/default': 0,
+  },
+};
